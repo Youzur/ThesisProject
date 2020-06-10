e809098b05fb2ff5f41bd60354ce7ed5eaf9e577
Youzur <yousefnamazi@hotmail.com>
2020-06-10 12:01:49 +0200

    Removed Test.js

--- src/package/Test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const foodName = 'Deluxe Pasta'
-const element = <p>Please, try our {foodName}</p>
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
